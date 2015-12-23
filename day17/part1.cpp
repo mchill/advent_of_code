45455925215236abe0fc7e61335109794ad9cb34
Matthew Hill <matthewchill7@gmail.com>
2015-12-22 22:29:41 -0500

    Days 17 and 18
    Getting closer...

---
+++ day17/part1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+
+using namespace std;
+
+string getChars() {
+    ifstream file("input.txt");
+    string input = "";
+    char c;
+
+    while (file.get(c))
+        input += c;
+
+    return input;
+}
+
+vector<vector<int> > getCombinations(int n) {
+    vector<vector<int> > combinations;
+    
+    for (int r = 1; r <= n; r++) {
+        vector<bool> v(n);
+        fill(v.begin() + n - r, v.end(), true);
+
+        do {
+            vector<int> combination;
+            
+            for (int i = 0; i < n; ++i) {
+                if (v[i]) {
+                    combination.push_back(i);
+                }
+            }
+            
+            combinations.push_back(combination);
+        } while (next_permutation(v.begin(), v.end()));
+    }
+    
+    return combinations;
+}
+
+int main () {
+    string file = getChars();
+    vector<int> containers;
+    
+    istringstream iss(file);
+    int value;
+
+    while (iss >> value)
+        containers.push_back(value);
+    
+    vector<vector<int> > combinations = getCombinations(containers.size());
+    int fits = 0;
+    
+    for(vector<vector<int> >::iterator combination = combinations.begin(); combination != combinations.end(); ++combination) {
+        int filled = 0;
+        
+        for(vector<int>::iterator container = (*combination).begin(); container != (*combination).end(); ++container) {
+            filled += containers[*container];
+            
+            if (filled > 150)
+                break;
+        }
+        
+        if (filled == 150)
+            fits++;
+    }
+    
+    cout << fits << endl;
+}
---
+++ day18/part1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+const int SIZE = 100;
+
+vector<string> getLines() {
+    ifstream file("input.txt");
+    vector<string> lines;
+    string line;
+
+    while (getline(file, line))
+        lines.push_back(line);
+
+    return lines;
+}
+
+int countNeighbors(bool *cell) {
+    return *(cell + 1) + *(cell + SIZE + 3) + *(cell + SIZE + 2) + *(cell + SIZE + 1) +
+            *(cell - 1) + *(cell - SIZE - 3) + *(cell - SIZE - 2) + *(cell - SIZE - 1);
+}
+
+int main () {
+    vector<string> lines = getLines();
+    bool grid1[SIZE + 2][SIZE + 2] = { false };
+    bool grid2[SIZE + 2][SIZE + 2] = { false };
+    
+    for(int y = 0; y < SIZE; y++) {
+        for (int x = 0; x < SIZE; x++) {
+            if (lines.at(y)[x] == '#') {
+                grid1[x+1][y+1] = true;
+                grid2[x+1][y+1] = true;
+            }
+        }
+    }
+    
+    for (int step = 0; step < 100; step++) {
+        for(int y = 1; y <= SIZE; y++) {
+            for (int x = 1; x <= SIZE; x++) {
+                int neighbors = countNeighbors(&(grid1[x][y]));
+                
+                if (grid1[x][y] && !(neighbors == 2 || neighbors == 3))
+                    grid2[x][y] = false;
+                else if (!grid1[x][y] && neighbors == 3)
+                    grid2[x][y] = true;
+            }
+        }
+        
+        for(int y = 1; y <= SIZE; y++)
+            for (int x = 1; x <= SIZE; x++)
+                grid1[x][y] = grid2[x][y];
+    }
+    
+    int on = 0;
+    
+    for(int y = 1; y <= SIZE; y++)
+        for (int x = 1; x <= SIZE; x++)
+            on += grid1[x][y];
+        
+    cout << on << endl;
+}
+
