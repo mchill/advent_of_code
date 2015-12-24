4878b143ef14dcdd21f90f8ce40b262459c98ebe
Matthew Hill <matthewchill7@gmail.com>
2015-12-23 20:02:35 -0500

    Days 22 and 23
    Days 21 and 22 were easy, probably because I play too many RPG's.
    I solved Day 21 without a program, but I'll probably go back and
    implement one.

---
+++ day22/part1.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+struct playerStruct {
+    int health;
+    int mana;
+    int usedMana;
+    int shield;
+    int poison;
+    int recharge;
+};
+
+struct bossStruct {
+    int health;
+    int damage;
+};
+
+int turn(playerStruct &player, bossStruct &boss) {
+    int record = 9000;
+    
+    // Apply effects
+    if (player.poison > 0)
+        boss.health -= 3;
+    if (player.recharge > 0)
+        player.mana += 101;
+    
+    // Decrement timers
+    player.shield = max(player.shield - 1, 0);
+    player.poison = max(player.poison - 1, 0);
+    player.recharge = max(player.recharge - 1, 0);
+    
+    for (int move = 0; move < 5; move++) {
+        playerStruct nextPlayer = player;
+        bossStruct nextBoss = boss;
+        
+        // Player casts a spell
+        if (move == 0) {
+            if (nextPlayer.mana < 53)
+                break;
+            
+            nextPlayer.mana -= 53;
+            nextPlayer.usedMana += 53;
+            nextBoss.health -= 4;
+        } else if (move == 1) {
+            if (nextPlayer.mana < 73)
+                break;
+            
+            nextPlayer.mana -= 73;
+            nextPlayer.usedMana += 73;
+            nextBoss.health -= 2;
+            nextPlayer.health += 2;
+        } else if (move == 2) {
+            if (nextPlayer.mana < 113)
+                break;
+            if (nextPlayer.shield > 0)
+                continue;
+            
+            nextPlayer.mana -= 113;
+            nextPlayer.usedMana += 113;
+            nextPlayer.shield = 6;
+        } else if (move == 3) {
+            if (nextPlayer.mana < 173)
+                break;
+            if (nextPlayer.poison > 0)
+                continue;
+            
+            nextPlayer.mana -= 173;
+            nextPlayer.usedMana += 173;
+            nextPlayer.poison = 6;
+        } else if (move == 4) {
+            if (nextPlayer.mana < 229)
+                break;
+            if (nextPlayer.recharge > 0)
+                continue;
+            
+            nextPlayer.mana -= 229;
+            nextPlayer.usedMana += 229;
+            nextPlayer.recharge = 5;
+        }
+        
+        // Apply effects
+        if (nextPlayer.poison > 0)
+            nextBoss.health -= 3;
+        if (nextPlayer.recharge > 0)
+            nextPlayer.mana += 101;
+        
+        // Decrement timers
+        nextPlayer.shield = max(nextPlayer.shield - 1, 0);
+        nextPlayer.poison = max(nextPlayer.poison - 1, 0);
+        nextPlayer.recharge = max(nextPlayer.recharge - 1, 0);
+        
+        if (nextBoss.health <= 0) {
+            record = min(record, nextPlayer.usedMana);
+            continue;
+        }
+    
+        // Boss turn
+        nextPlayer.health -= (10 - (nextPlayer.shield > 0) * 7);
+        if (nextPlayer.health <= 0)
+            continue;
+    
+        // Next turn
+        record = min(record, turn(nextPlayer, nextBoss));
+    }
+    
+    return record;
+}
+
+int main () {
+    playerStruct player;
+    player.health = 50;
+    player.mana = 500;
+    player.usedMana = 0;
+    player.shield = 0;
+    player.poison = 0;
+    player.recharge = 0;
+    
+    bossStruct boss;
+    boss.health = 71;
+    boss.damage = 10;
+    
+    cout << turn(player, boss) << endl;
+}
+
---
+++ day23/part2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+vector<string> getLines() {
+    ifstream file("input.txt");
+    vector<string> lines;
+    string line;
+
+    while (getline(file, line))
+        lines.push_back(line);
+
+    return lines;
+}
+
+int main () {
+    vector<string> lines = getLines();
+    vector<vector<int> > instructions;
+    int registers[] = {1, 0};
+    
+    for(vector<string>::iterator line = lines.begin(); line != lines.end(); ++line) {
+        replace((*line).begin(), (*line).end(), ',', ' ');
+        replace((*line).begin(), (*line).end(), '+', ' ');
+        
+        istringstream iss1(*line), iss2(*line), iss3(*line);
+        string instruction;
+        char reg = 0;
+        int jump = 0;
+
+        if (!(iss1 >> instruction >> jump))
+            iss2 >> instruction >> reg >> jump;
+        
+        int instructionIndex;
+        if (instruction == "hlf") instructionIndex = 0;
+        else if (instruction == "tpl") instructionIndex = 1;
+        else if (instruction == "inc") instructionIndex = 2;
+        else if (instruction == "jmp") instructionIndex = 3;
+        else if (instruction == "jie") instructionIndex = 4;
+        else if (instruction == "jio") instructionIndex = 5;
+        
+        instructions.push_back(vector<int> {instructionIndex, reg, jump});
+    }
+    
+    for (int i = 0; i < instructions.size(); ) {
+        char reg = instructions[i].at(1);
+        
+        switch (instructions[i].at(0)) {
+            case 0:
+                registers[reg - 'a'] /= 2;
+                i++;
+                break;
+            case 1:
+                registers[reg - 'a'] *= 3;
+                i++;
+                break;
+            case 2:
+                registers[reg - 'a']++;
+                i++;
+                break;
+            case 3:
+                i += instructions[i].at(2);
+                break;
+            case 4:
+                if (registers[reg - 'a'] % 2 == 0)
+                    i += instructions[i].at(2) - 1;
+                i++;
+                break;
+            case 5:
+                if (registers[reg - 'a'] == 1)
+                    i += instructions[i].at(2) - 1;
+                i++;
+        }
+    }
+    
+    cout << registers[1] << endl;
+}
