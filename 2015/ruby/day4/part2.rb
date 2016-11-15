67da0c394f9cd1604affbc6b5aee9aee70cd0c94
Matthew Hill <matthewchill7@gmail.com>
2016-11-15 11:38:41 -0500

    2015 Day 4 in Ruby

---
+++ 2015/ruby/day4/part2.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+require 'digest'
+
+input = 'ckczppom'
+number = 1
+hash = Digest::MD5.hexdigest(input + number.to_s)
+
+until hash.start_with?('000000')
+    number += 1
+    hash = Digest::MD5.hexdigest(input + number.to_s)
+end
+
+puts number
