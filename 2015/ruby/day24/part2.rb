9fe35cfee7139b4b4253e0169e2ca1fc157aa7b4
Matthew Hill <matthewchill7@gmail.com>
2016-12-02 10:35:47 -0500

    2015 Day 24 in Ruby
    I made the assumption that the remainder after the first group
    would be evenly divisible for the remaining groups. Seems like
    that was an unmentioned criteria for generating input.

---
+++ 2015/ruby/day24/part2.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+begin
+    File.open('input.txt', 'r') do |file|
+        packages = []
+
+        file.each_line do |line|
+            packages.push(line.to_i)
+        end
+
+        fourth = packages.inject(:+) / 4
+        min_entanglement = nil
+
+        for first_size in 1..packages.count
+            first_groups = packages.combination(first_size)
+
+            first_groups.each do |first_group|
+                if first_group.inject(:+) != fourth then next end
+
+                entanglement = first_group.inject(:*)
+
+                if min_entanglement.nil? or entanglement < min_entanglement
+                    min_entanglement = entanglement
+                end
+            end
+
+            if not min_entanglement.nil? then break end
+        end
+
+        puts min_entanglement
+    end
+rescue SystemCallError
+    puts 'input.txt not found.'
+end
