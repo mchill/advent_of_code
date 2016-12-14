a8e4d4ab427764cd0c3e6b9c964045d15fe9d8b3
Matthew Hill <matthewchill7@gmail.com>
2016-12-14 10:46:36 -0500

    2016 Day 11 in Python
    Easily the hardest day this year. Might come back to optimize it
    another day.

---
+++ 2016/python/day11/part1.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+from itertools import combinations
+from collections import deque
+
+
+PAIRS = 5
+
+
+class Generator:
+    def __init__(self, element):
+        self.element = element
+
+
+class Microchip:
+    def __init__(self, element, match):
+        self.element = element
+        self.match = match
+
+
+class State:
+    def __init__(self, floor, floors, csf):
+        self.floor = floor
+        self.floors = floors
+        self.csf = csf
+        self.key = self.key()
+
+    def key(self):
+        key = []
+
+        for element in range(PAIRS):
+            key.append((
+                next(index for index, floor in enumerate(self.floors) if any(
+                    isinstance(part, Generator) and part.element == element for part in floor
+                )),
+                next(index for index, floor in enumerate(self.floors) if any(
+                    isinstance(part, Microchip) and part.element == element for part in floor
+                ))
+            ))
+
+        return (self.floor, tuple(sorted(key)))
+
+    def moves(self, traversed):
+        combos = (list(combinations(self.floors[self.floor], 2)) +
+                  list(combinations(self.floors[self.floor], 1)))
+        valid_moves = []
+
+        for direction in [1, -1]:
+            next_floor = self.floor + direction
+            if not 0 <= next_floor < 4:
+                continue
+
+            for combo in combos:
+                floors = [list(floor) for floor in self.floors]
+
+                for part in combo:
+                    floors[next_floor].append(part)
+                    floors[self.floor].remove(part)
+
+                valid_moves.append(State(next_floor, floors, self.csf + 1))
+
+        return [move for move in valid_moves if move.valid(traversed)]
+
+    def valid(self, traversed):
+        if self.key in traversed:
+            traversed[self.key].csf = min(traversed[self.key].csf, self.csf)
+            return False
+
+        for floor in self.floors:
+            if (any(isinstance(part, Microchip) and part.match not in floor for part in floor) and
+                any(isinstance(part, Generator) for part in floor)):
+                return False
+
+        traversed[self.key] = self
+        return True
+
+
+def main():
+    gen = []
+    chip = []
+
+    for pair in range(PAIRS):
+        gen.append(Generator(pair))
+        chip.append(Microchip(pair, gen[pair]))
+
+    initial = State(0, [
+        [gen[0], chip[0]],
+        [gen[1], gen[2], gen[3], gen[4]],
+        [chip[1], chip[2], chip[3], chip[4]],
+        []
+    ], 0)
+
+    traversed = {}
+    possible = deque([initial])
+
+    while len(possible):
+        current = possible.popleft()
+
+        if len(current.floors[0]) + len(current.floors[1]) + len(current.floors[2]) == 0:
+            print current.csf
+            break
+
+        possible.extend(current.moves(traversed))
+
+
+if __name__ == "__main__":
+    main()
