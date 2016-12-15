7ed0ac1ee07d9db5c297c29761c6a7702310df46
Matthew Hill <matthewchill7@gmail.com>
2016-12-15 11:18:38 -0500

    2016 Day 14 in Python

---
+++ 2016/python/day14/part2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import hashlib
+import re
+
+
+class Match:
+    def __init__(self, index, search):
+        self.index = index
+        self.search = search
+        self.found = False
+
+
+matches = []
+triple = re.compile(r'(\w)\1\1')
+
+keys = 0
+index = -1
+
+while keys < 64:
+    index += 1
+    key = hashlib.md5('zpqevtbw' + str(index)).hexdigest()
+
+    for _ in range(2016):
+        key = hashlib.md5(key).hexdigest()
+
+    matches[:] = [match for match in matches if not match.found and index - match.index <= 1000]
+    for match in matches:
+        if match.search in key:
+            match.found = True
+            keys += 1
+
+            if keys == 64:
+                print match.index
+                break
+
+    match = re.search(triple, key)
+    if match:
+        matches.append(Match(index, match.group(1)[0] * 5))
