e976e9e4f8b26ddba9ec3f2f79046aac15fdddaa
Matthew Hill <matthewchill7@gmail.com>
2016-12-16 10:45:19 -0500

    2016 Day 16 in Python
    List comprehension is great.

---
+++ 2016/python/day16/part2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+data = '10010000000110000'
+length = 35651584
+
+while len(data) < length:
+    data += '0' + ''.join('1' if char == '0' else '0' for char in data[::-1])
+
+checksum = data[:length]
+
+while len(checksum) % 2 == 0:
+    checksum = ['1' if checksum[index] == checksum[index + 1]
+                else '0' for index in range(0, len(checksum), 2)]
+
+print ''.join(checksum)
