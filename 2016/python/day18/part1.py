2b5befd719b56a3ba6bc0e6bcb76fe92ba2f591a
Matthew Hill <matthewchill7@gmail.com>
2016-12-20 20:07:28 -0500

    2016 Day 18 in Python

---
+++ 2016/python/day18/part1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+with open('input.txt') as file_handler:
+    row = '.' + file_handler.read().rstrip() + '.'
+
+safe = row.count('.') - 2
+traps = ['^^.', '.^^', '^..', '..^']
+
+for _ in range(39):
+    next_row = '.'
+
+    for index in range(1, len(row) - 1):
+        next_row += '^' if row[index - 1: index + 2] in traps else '.'
+
+    row = next_row + '.'
+    safe += row.count('.') - 2
+
+print safe
