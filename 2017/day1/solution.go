46e1719e1b375dcdcf029c082169af1da454bf0f
Matthew Hill <matthewchill7@gmail.com>
2017-12-01 05:50:05 -0500

    2017 Day 1
    Another year, another Christmas adventure! I'll be learning Go
    throughout the month, so I don't expect to be too competitive on
    the leaderboard. I miss Python already.

---
+++ 2017/day1/solution.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("input.txt")
+
+	if err != nil {
+		fmt.Println("Error reading input.txt")
+		panic(err)
+	}
+
+	length := len(data) - 1
+	data = data[:length]
+	part1 := 0
+	part2 := 0
+
+	for index := 0; index < length; index++ {
+		if data[index] == data[(index+1)%length] {
+			part1 += int(data[index]) - '0'
+		}
+		if data[index] == data[(index+length/2)%length] {
+			part2 += int(data[index]) - '0'
+		}
+	}
+
+	fmt.Println("Part1:", part1)
+	fmt.Println("Part2:", part2)
+}
+
